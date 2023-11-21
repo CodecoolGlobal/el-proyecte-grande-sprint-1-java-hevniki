ba60ab8c33a8b3aba418c74a70410c66247390e2
naaaani <nandi.zalka@gmail.com>
2023-11-21 14:43:04 +0100

    Add dummy data of recipes.

--- backend/src/main/java/com/codecool/cookpad/service/dao/RecipeDAO.java
+++ backend/src/main/java/com/codecool/cookpad/service/dao/RecipeDAO.java
@@ -1,4 +1,11 @@
 package com.codecool.cookpad.service.dao;
+import com.codecool.cookpad.model.Recipe;
+
+import java.util.Set;
+import java.util.UUID;
 
 public interface RecipeDAO {
+    Set<Recipe> getRecipes();
+    //boolean addRecipe(Recipe recipe);
+    //Recipe getRecipe(UUID id);
 }
