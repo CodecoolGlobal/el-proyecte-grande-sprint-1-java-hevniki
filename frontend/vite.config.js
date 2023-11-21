57858e828aa2b8f1c30007d96c4e79a0da6eb660
szbubu <szbubu@gmail.com>
2023-11-21 13:47:03 +0100

    set up proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://localhost:8080',
+        target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
         ws: true,
