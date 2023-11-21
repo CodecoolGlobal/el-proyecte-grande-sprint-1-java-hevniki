96fefe40346e1c433e6187e9869069f36b0f2ad9
szbubu <szbubu@gmail.com>
2023-11-21 21:25:39 +0100

    implement endpoint and methods in dao, service

--- backend/src/main/java/com/codecool/cookpad/service/dao/IngredientDAO.java
+++ backend/src/main/java/com/codecool/cookpad/service/dao/IngredientDAO.java
@@ -3,7 +3,9 @@ package com.codecool.cookpad.service.dao;
 import com.codecool.cookpad.model.Ingredient;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IngredientDAO {
     List<Ingredient> getAllIngredients();
+    Optional<Ingredient> getIngredientById(String id);
 }
