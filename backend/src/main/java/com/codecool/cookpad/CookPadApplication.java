b63b9148ffc543fc8ecfc430a3e08b6847161d56
Nikolett Hevizi <nikihevizi@gmail.com>
2023-11-21 09:14:26 +0100

    stg

--- backend/src/main/java/com/codecool/cookpad/CookPadApplication.java
+++ backend/src/main/java/com/codecool/cookpad/CookPadApplication.java
@@ -1,13 +1,13 @@
-package com.codecool.backend;
+package com.codecool.cookpad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class CookPadApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(CookPadApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/codecool/cookpad/BackendApplicationTests.java
+++ backend/src/test/java/com/codecool/cookpad/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.codecool.backend;
+package com.codecool.cookpad;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
