8412dc27266861a2cffa0788b0a7d9b37827456c
naaaani <nandi.zalka@gmail.com>
2023-11-22 11:21:13 +0100

    Add List of Recipe names

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,11 @@
+import RecipeList from "./RecipeList";
 
 
 function App() {
 
   return (
     <>
-      <div>Hello</div>
-      <button onClick={async () => {
-        const res = await fetch("/api");
-        const dummy = await res.json();
-        console.log(dummy);
-      }}>Click me!</button>
+      <RecipeList></RecipeList>
     </>
   )
 }
