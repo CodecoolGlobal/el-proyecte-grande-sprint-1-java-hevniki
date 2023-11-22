4b8faf268cd3439b8fc29f0c8b6ca56d342fd062
szbubu <szbubu@gmail.com>
2023-11-22 23:10:54 +0100

    implement necessary methods in service and dao

--- backend/src/main/java/com/codecool/cookpad/service/dao/IngredientDAO.java
+++ backend/src/main/java/com/codecool/cookpad/service/dao/IngredientDAO.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface IngredientDAO {
     List<Ingredient> getAllIngredients();
     Optional<Ingredient> getIngredientById(String id);
+    public boolean addIngredient(Ingredient ingredient);
 }
