4cec352d3fba815ca518b3757bedc5f7bea73223
naaaani <nandi.zalka@gmail.com>
2023-11-22 12:54:48 +0100

    Fix Typos in Frontend Routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -22,6 +22,7 @@ const router = createBrowserRouter([
                 element: <AllIngredients/>,
             },
             {
+
                 path: "/ingredients/:id",
                 element: <IngredientDetails/>,
             },
