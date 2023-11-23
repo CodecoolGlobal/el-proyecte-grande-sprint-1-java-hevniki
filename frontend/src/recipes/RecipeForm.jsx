f4f1ce5c4d83fd90ed5c52b326c9b82e5605f3a1
naaaani <nandi.zalka@gmail.com>
2023-11-23 11:39:15 +0100

    Save Progress before lunch

--- frontend/src/recipes/RecipeForm.jsx
+++ frontend/src/recipes/RecipeForm.jsx
@@ -9,6 +9,9 @@ async function fetchIngredients() {
 
 function RecipeForm() {
     const [ingredients, setIngredients] = useState(null);
+    const [filteredIngredients, setFilteredIngredients] = useState([]);
+    const [filterValue, setFilterValue] = useState('');
+    const [selectedIngredients, setSelectedIngredients] = useState([]);
 
     useEffect(() => {
         async function task() {
@@ -24,9 +27,31 @@ function RecipeForm() {
         document.getElementById("myDropdown").classList.toggle("show");
     }
 
+    function handleChange(event) {
+        setFilterValue(event.target.value);
+        if (event.target.value.length > 2) {
+            filterIngredients();
+        } 
+    }
+
+    function filterIngredients() {
+        setFilteredIngredients(ingredients.filter((ingredient) => {
+            return ingredient.name.substring(0, filterValue.length).toLowerCase() === filterValue.toLowerCase();
+        }));
+    }
+
+    function handleIngredientSelect(event) {
+        event.preventDefault();
+        const id = event.target.id;
+        console.log(event.target.id);
+        console.log(id);
+        setSelectedIngredients([...selectedIngredients, id]);
+    }
+
     if (ingredients == null) {
         return <>Loading...</>;
     }
+
     return (
         <form>
             <label htmlFor='recipeName'>Name: </label>
@@ -35,9 +60,9 @@ function RecipeForm() {
             <div className="dropdown">
                 <button className="dropbtn" onClick={showOptions}>Dropdown</button>
                 <div id="myDropdown" className="dropdown-content">
-                    <input type="text" placeholder="Search.." id="myInput"/>
-                        {ingredients.map(ingredient => {
-                            return <a>{ingredient.name}</a>
+                    <input type="text" placeholder="Search.." id="myInput" onChange={handleChange}/>
+                        {filteredIngredients.map(ingredient => {
+                            return <a id={ingredient.id} onClick={handleIngredientSelect}>{ingredient.name}</a>
                         })}
                 </div>
             </div>
