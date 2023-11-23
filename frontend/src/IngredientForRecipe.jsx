09ad56fa91206fa8103397543a537e95853cc68b
szbubu <szbubu@gmail.com>
2023-11-23 14:08:12 +0100

    refactor recipeDTO

--- frontend/src/IngredientForRecipe.jsx
+++ frontend/src/IngredientForRecipe.jsx
@@ -5,65 +5,14 @@ import {useEffect, useState} from "react";
 
 
 function IngredientForRecipe (props) {
-   /*
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
-    const [ingredient, setIngredient] = useState(null);
-
-    */
-    const id = props.props[0];
-    const name = props.props[1];
-    const amount = props.props[2];
-    const unit = props.props[3];
+   const { id, name, amount, unitOfMeasure} = props.ingredient;
 
 
-   /* async function fetchIngredient(id) {
-        try {
-            const res = await fetch(`/api/ingredients/${id}`);
-
-            if (!res.ok) {
-                if (res.status === 404) {
-                    throw new Error("Ingredient Not Found");
-                }
-            }
-            return res.json();
-        } catch (error) {
-            setError(error.message);
-
-        }
-    }
-    useEffect(() => {
-        async function task() {
-            const ingredient = await fetchIngredient(id);
-            setIngredient(ingredient);
-            setLoading(false);
-        }
-        task();
-
-    }, []);
-
-    if (error) {
-        return (
-            <div>
-                404 error, page not found
-            </div>
-        )
-    }
-
-    if (loading) {
-        return (
-            <div>
-                loading
-            </div>
-        )
-    }
-
-    */
     return (
         <>
             <ul>
                 <li>
-                    {amount} {unit} of {name}
+                    {amount} {unitOfMeasure} of {name}
                 </li>
 
                 <Link to={`/ingredients/${id}`}>
--- frontend/src/RecipeDetails.jsx
+++ frontend/src/RecipeDetails.jsx
@@ -50,13 +50,15 @@ function RecipeDetails() {
                 <li>is Vegetarian : {(recipe.isVegetarian) ? "yes" : "no"}</li>
                 <li>is GlutenFree : {(recipe.isGlutenFree) ? "yes" : "no"}</li>
                 <li>is DairyFree : {(recipe.isDairyFree) ? "yes" : "no"}</li>
-                <div>Ingredients:</div>
-                <>{recipe.ingredients.map((ingredient) => {
+                <div>Ingredients: </div>
+                <>{recipe.ingredients.map((ingredient)=>{
+                    console.log(ingredient);
                     return <IngredientForRecipe
-                        key={ingredient[0]}
-                        props={ingredient}
-                    />
-                })}</>
+                        key = {ingredient.id}
+                        ingredient={ingredient}
+                />})}</>
+
+
             </ul>
             <button onClick={async () => {
                 await deleteRecipe(id);
