e41ac5872fef51b8e4de13370cf16341f83beb4d
naaaani <nandi.zalka@gmail.com>
2023-11-23 09:38:39 +0100

    Add links to all sites

---
+++ frontend/src/Layout.jsx
@@ -0,0 +1,24 @@
+import {Link, Outlet} from "react-router-dom";
+
+function Layout(){
+    return(
+        <div className="Layout">
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/recipes">Recipes</Link>
+                    </li>
+                    <li>
+                        <Link to="/ingredients">Ingredients</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Outlet/>
+        </div>
+    )
+}
+
+export default Layout;
