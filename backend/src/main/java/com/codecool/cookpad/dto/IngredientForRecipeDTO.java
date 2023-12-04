0542b00beabe66b39be5357b912503fe315b2564
szbubu <szbubu@gmail.com>
2023-12-04 14:50:10 +0100

    update DTOs

--- backend/src/main/java/com/codecool/cookpad/dto/IngredientForRecipeDTO.java
+++ backend/src/main/java/com/codecool/cookpad/dto/IngredientForRecipeDTO.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record IngredientForRecipeDTO(
+        Long id,
         IngredientTypeDTO ingredient,
         double amount) {
 }
