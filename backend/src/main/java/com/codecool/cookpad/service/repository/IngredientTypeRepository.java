bdc73038462f3c52c16694588fd6477034f840fd
szbubu <szbubu@gmail.com>
2023-12-04 13:34:52 +0100

    create repositories

---
+++ backend/src/main/java/com/codecool/cookpad/service/repository/IngredientTypeRepository.java
@@ -0,0 +1,11 @@
+package com.codecool.cookpad.service.repository;
+
+import com.codecool.cookpad.model.IngredientType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface IngredientTypeRepository extends JpaRepository<IngredientType, Long> {
+    Optional<IngredientType> findByName(String name);
+
+}
