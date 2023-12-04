6805061c130ac1ec6d4d80188f07d6d8aa6a6540
szbubu <szbubu@gmail.com>
2023-12-04 18:27:47 +0100

    work in progress commit

--- backend/src/main/java/com/codecool/cookpad/service/repository/IngredientForRecipeRepository.java
+++ backend/src/main/java/com/codecool/cookpad/service/repository/IngredientForRecipeRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface IngredientForRecipeRepository extends JpaRepository<IngredientForRecipe, Long> {
-    Optional<IngredientForRecipe> findByName(String name);
 
 }
 
