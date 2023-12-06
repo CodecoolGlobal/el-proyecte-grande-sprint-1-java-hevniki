1d5a2220bfdb8e6563304c708c6fee8b59d0ba59
szbubu <szbubu@gmail.com>
2023-12-06 13:29:25 +0100

    add case-insensitive filtering for name for recipes for partial matches

--- backend/src/main/java/com/codecool/cookpad/service/repository/IngredientTypeRepository.java
+++ backend/src/main/java/com/codecool/cookpad/service/repository/IngredientTypeRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface IngredientTypeRepository extends JpaRepository<IngredientType, Long> {
     Optional<IngredientType> findByName(String name);
 
+
 }
--- backend/src/main/java/com/codecool/cookpad/service/repository/RecipeRepository.java
+++ backend/src/main/java/com/codecool/cookpad/service/repository/RecipeRepository.java
@@ -1,13 +1,14 @@
 package com.codecool.cookpad.service.repository;
 
-import com.codecool.cookpad.model.IngredientType;
 import com.codecool.cookpad.model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
     Optional<Recipe> findByName(String name);
+    List<Recipe> findByNameContainingIgnoreCase(String name);
 
 }
 
