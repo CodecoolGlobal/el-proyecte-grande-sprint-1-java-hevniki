992dc330426974bc931e0c22207687581a6a3c88
szbubu <szbubu@gmail.com>
2023-12-06 13:29:25 +0100

    add case-insensitive filtering for name for recipes for partial matches

--- backend/src/main/java/com/codecool/cookpad/service/IngredientTypeService.java
+++ backend/src/main/java/com/codecool/cookpad/service/IngredientTypeService.java
@@ -1,7 +1,8 @@
 package com.codecool.cookpad.service;
 
 import com.codecool.cookpad.dto.IngredientTypeDTO;
-import com.codecool.cookpad.model.IngredientNotFoundException;
+
+import com.codecool.cookpad.exception.IngredientNotFoundException;
 import com.codecool.cookpad.model.IngredientType;
 import com.codecool.cookpad.service.repository.IngredientTypeRepository;
 import org.springframework.stereotype.Service;
