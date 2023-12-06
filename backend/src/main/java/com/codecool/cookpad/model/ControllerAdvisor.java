6793d8027fc1083cbc6d5c1646de62a05b81ab0b
szbubu <szbubu@gmail.com>
2023-12-06 11:11:37 +0100

    add and throw custom exception when data not found

---
+++ backend/src/main/java/com/codecool/cookpad/model/ControllerAdvisor.java
@@ -0,0 +1,23 @@
+package com.codecool.cookpad.model;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+@ControllerAdvice
+public class ControllerAdvisor {
+    @ResponseBody
+    @ExceptionHandler(RecipeNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String recipeNotFoundException(RecipeNotFoundException ex) {
+
+        return ex.getMessage();
+    }
+    @ResponseBody
+    @ExceptionHandler(IngredientNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String IngredientNotFoundException(IngredientNotFoundException ex) {
+
+        return ex.getMessage();
+    }
+}
