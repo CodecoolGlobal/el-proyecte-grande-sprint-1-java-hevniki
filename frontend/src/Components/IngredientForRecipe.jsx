72d133beab36e5f3fc1d35623baeff0f35a47238
naaaani <nandi.zalka@gmail.com>
2023-12-07 11:15:04 +0100

    Fix IngretientForRecipe component

--- frontend/src/Components/IngredientForRecipe.jsx
+++ frontend/src/Components/IngredientForRecipe.jsx
@@ -1,24 +1,14 @@
-import {Link} from "react-router-dom";
-
 function IngredientForRecipe(props) {
-    const {id, ingredient, amount} = props.ingredient;
+	const { id, ingredient, amount } = props.ingredient;
 
-
-    return (
-        <>
-            <ul>
-                <li>
-                    {amount} {ingredient.unitOfMeasure} of {ingredient.name.toLowerCase()}
-                </li>
-
-                <Link to={`/ingredients/${id}`}>
-                    <button>
-                        show more details of this ingredient
-                    </button>
-                </Link>
-            </ul>
-        </>
-    )
+	return (
+		<>
+			<li>
+				{amount} {ingredient.unitOfMeasure} of {ingredient.name.toLowerCase()}
+			</li>
+			<hr></hr>
+		</>
+	)
 }
 
 export default IngredientForRecipe;
