4ebeaf5ee79d883f2953db2a962a463dd5fc61a9
naaaani <nandi.zalka@gmail.com>
2024-01-03 14:50:47 +0100

    Add handleSubmit function.

--- frontend/src/Pages/LoginForm/LoginForm.jsx
+++ frontend/src/Pages/LoginForm/LoginForm.jsx
@@ -4,6 +4,16 @@ import "./LoginForm.css";
 function LoginForm() {
 	const [errorMessages, setErrorMessages] = useState({});
 	const [isSubmitted, setIsSubmitted] = useState(false);
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+
+	function handleNameChange(event) {
+		setUsername(event.target.value);
+	}
+
+	function handlePasswordChange(event) {
+		setPassword(event.target.value);
+	}
 
 	function renderErrorMessage(name) {
 		name === errorMessages.name && (
@@ -13,20 +23,29 @@ function LoginForm() {
 
 	function handleSubmit(event) {
 		event.preventDefault();
-	}
 
+		const authenticationRequest = {
+			username: username,
+			password: password
+		}
+
+		const res = fetch("/api/auth/authenticate", {
+			method: "POST",
+			body: JSON.stringify(authenticationRequest),
+		});
+	}
 
 	return (
 		<div className="form">
 			<form onSubmit={handleSubmit}>
 				<div className="input-container">
 					<label>Username </label>
-					<input type="text" name="uname" required />
+					<input type="text" name="uname" onInput={handleNameChange} required />
 					{renderErrorMessage("uname")}
 				</div>
 				<div className="input-container">
 					<label>Password </label>
-					<input type="password" name="pass" required />
+					<input type="password" name="pass" onInput={handlePasswordChange} required />
 					{renderErrorMessage("pass")}
 				</div>
 				<div className="button-container">
@@ -36,4 +55,4 @@ function LoginForm() {
 		</div>)
 }
 
-export default LoginForm
+export default LoginForm;
