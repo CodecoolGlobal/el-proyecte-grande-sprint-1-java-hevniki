0e958eb582d873a3d0b8bde35baca3b87f588dfb
szbubu <szbubu@gmail.com>
2024-01-03 09:35:26 +0100

    try to set up security

--- backend/src/main/java/com/codecool/cookpad/model/entity/User.java
+++ backend/src/main/java/com/codecool/cookpad/model/entity/User.java
@@ -1,24 +1,57 @@
 package com.codecool.cookpad.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-@Setter
-@Getter
+import java.util.Collection;
+import java.util.List;
+
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode
+@Builder
+@Table(name =" _user")
 
 @Entity
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue
     private Long id;
-    private String userName;
+    private String username;
     private String password;
-    @OneToOne
+   @Enumerated(EnumType.STRING)
     private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
