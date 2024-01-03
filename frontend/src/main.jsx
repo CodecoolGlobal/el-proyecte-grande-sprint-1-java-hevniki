e61fc37a91babeebed47ba9310a32b2cc4adaf9c
naaaani <nandi.zalka@gmail.com>
2024-01-03 13:27:23 +0100

    Add LoginForm skeleton

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -35,7 +35,6 @@ const router = createBrowserRouter([
                 element: <IngredientList/>,
             },
             {
-
                 path: "/ingredients/:id",
                 element: <IngredientDetails/>,
             },
