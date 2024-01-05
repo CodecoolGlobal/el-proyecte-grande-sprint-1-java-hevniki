90c449775c27f39d5f2b3de6ab180f65b242b029
naaaani <nandi.zalka@gmail.com>
2024-01-05 11:32:11 +0100

    Add fluff to home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 function App() {
 
   return (
-    <>
-      Well met!
-    </>
+    <footer>
+      <div class="mx-auto max-w-main-content-wide px-md">
+        <p class="text-cookpad-14 mb-sm font-semibold">
+          <h3>A Cookpadról</h3>
+        </p>
+        <p class="text-cookpad-16 leading-7">
+          A Cookpadnál a missziónk, hogy <span class="font-semibold">a mindennapi főzést szórakoztatóvá tegyük</span>. Mert hiszünk benne, hogy a sütés-főzés az egyik kulcsa a boldogabb és egészségesebb életnek, közösségnek és bolygónak. Lehetővé tesszük az emberek számára mindenütt a világon, hogy így <span class="font-semibold">segítsenek egymásnak</span> a receptek és konyhai tippek megosztásával.
+        </p>
+      </div>
+    </footer>
   )
 }
 
