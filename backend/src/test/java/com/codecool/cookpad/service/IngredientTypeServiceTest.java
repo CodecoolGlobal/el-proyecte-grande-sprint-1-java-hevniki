3075cef3605670bf66bbf1b94fc8da3b5c98b073
szbubu <szbubu@gmail.com>
2024-04-11 13:05:56 +0200

    Add more tests for ingredinetTypeService

--- backend/src/test/java/com/codecool/cookpad/service/IngredientTypeServiceTest.java
+++ backend/src/test/java/com/codecool/cookpad/service/IngredientTypeServiceTest.java
@@ -25,14 +25,15 @@ class IngredientTypeServiceTest {
     @InjectMocks
     private IngredientTypeService ingredientTypeService;
 
-    private IngredientType ingredient;
     private IngredientType ingredient1;
+    private IngredientType ingredient2;
+    private IngredientType ingredient3;
 
     @BeforeEach
     public void setup() {
         ingredientTypeRepository = mock(IngredientTypeRepository.class);
         ingredientTypeService = new IngredientTypeService(ingredientTypeRepository);
-        ingredient = IngredientType.builder()
+        ingredient1 = IngredientType.builder()
                 .id(1L)
                 .name("Salt")
                 .isDairyFree(true)
@@ -42,7 +43,7 @@ class IngredientTypeServiceTest {
                 .unitOfMeasure("g")
                 .build();
 
-        ingredient1 = IngredientType.builder()
+        ingredient2 = IngredientType.builder()
                 .id(2L)
                 .name("Sugar")
                 .isDairyFree(true)
@@ -51,34 +52,54 @@ class IngredientTypeServiceTest {
                 .isEggFree(true)
                 .unitOfMeasure("g")
                 .build();
+        ingredient3 = IngredientType.builder()
+                .id(3L)
+                .name("Water")
+                .isDairyFree(true)
+                .isGlutenFree(true)
+                .isMeatFree(true)
+                .isEggFree(true)
+                .unitOfMeasure("l")
+                .build();
     }
 
-    @DisplayName("Test for getAllEmployees method")
+    @DisplayName("Test for findAll method")
     @Test
-    public void givenIngredientList_whenGetAllIngredients_thenReturnIngredients() {
-        given(ingredientTypeRepository.findAll()).willReturn(List.of(ingredient, ingredient1));
+    public void testFindAll_Expected() {
+        given(ingredientTypeRepository.findAll()).willReturn(List.of(ingredient1, ingredient2, ingredient3));
 
         List<IngredientTypeDTO> ingredientList = ingredientTypeService.getAllIngredients();
 
         assertThat(ingredientList).isNotNull();
-        assertThat(ingredientList.size()).isEqualTo(2);
+        assertThat(ingredientList.size()).isEqualTo(3);
     }
 
     @DisplayName("Test for mapToDTO method")
     @Test
-    public void givenIngredient_whenMapToDTO_thenReturnIngredientDTO() {
+    public void testMapToDTO_expected() {
         IngredientTypeDTO expected = new IngredientTypeDTO(1L, "Salt", "g", true, true, true, true);
-        IngredientTypeDTO actual = ingredientTypeService.mapToDTO(ingredient);
+        IngredientTypeDTO actual = ingredientTypeService.mapToDTO(ingredient1);
 
+        assertEquals(expected, actual);
+    }
+
+    @DisplayName("Test for mapFromDTO method")
+    @Test
+    public void testMapFromDTO_expected() {
+        given(ingredientTypeRepository.findById(ingredient1.getId())).willReturn(Optional.of(ingredient1));
+
+        IngredientType expected = ingredient1;
+        IngredientType actual = ingredientTypeService.mapFromDTO(new IngredientTypeDTO(1L, "Salt", "g", true, true, true, true));
         assertEquals(expected, actual);
     }
 
     @DisplayName("Test for getIngredientById method")
     @Test
     public void givenIngredientId_whenGetIngredientById_thenReturnIngredientDTO() {
-        given(ingredientTypeRepository.findById(ingredient.getId())).willReturn(Optional.of(ingredient));
+        given(ingredientTypeRepository.findById(ingredient1.getId())).willReturn(Optional.of(ingredient1));
 
         IngredientTypeDTO expected = new IngredientTypeDTO(1L, "Salt", "g", true, true, true, true);
         IngredientTypeDTO actual = ingredientTypeService.getIngredientById("1");
+        assertEquals(expected, actual);
     }
 }
