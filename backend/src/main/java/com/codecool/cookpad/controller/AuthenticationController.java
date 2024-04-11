10bdda5541dc803fdfd9c767c9acbd2532740154
naaaani <122887132+naaaani@users.noreply.github.com>
2024-04-11 10:27:22 +0200

    Remove Log

--- backend/src/main/java/com/codecool/cookpad/controller/AuthenticationController.java
+++ backend/src/main/java/com/codecool/cookpad/controller/AuthenticationController.java
@@ -28,7 +28,6 @@ public class AuthenticationController {
     public ResponseEntity<AuthenticationResponse> authenticate(
             @RequestBody AuthenticationRequest request
     ) {
-        System.out.println(request);
         return ResponseEntity.ok(service.authenticate(request));
     }
 
