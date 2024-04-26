6f575bae6424db21638b455086103b7366110fee
naaaani <nandi.zalka@gmail.com>
2024-04-26 13:42:26 +0200

    Permit all swagger related requests

--- backend/src/main/java/com/codecool/cookpad/security/SecurityConfig.java
+++ backend/src/main/java/com/codecool/cookpad/security/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("/v3/**", "/swagger-ui/**").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/api/recipes").hasRole("USER")
