50bf52caa02ac723bc8231e21d43fc89a8653d08
naaaani <nandi.zalka@gmail.com>
2024-05-08 13:20:29 +0200

    Remove redundant function call from createRecipe method.

--- backend/src/main/java/com/codecool/cookpad/service/RecipeService.java
+++ backend/src/main/java/com/codecool/cookpad/service/RecipeService.java
@@ -53,7 +53,7 @@ public class RecipeService {
     public void createRecipe(RecipeDTO newRecipeDTO) {
         Recipe recipe = mapFromDTO(newRecipeDTO);
         recipe.setProperties();
-        this.recipeRepository.save(mapFromDTO(newRecipeDTO));
+        this.recipeRepository.save(recipe);
     }
 
     public void updateRecipe(RecipeDTO updatedRecipeDTO) {
@@ -105,7 +105,7 @@ public class RecipeService {
             mappedIngredientForRecipe.setId(ingredientForRecipeDTO.id());
         }
         mappedIngredientForRecipe.setAmount(ingredientForRecipeDTO.amount());
-        mappedIngredientForRecipe.setIngredientType(this.ingredientTypeService.mapFromDTO(ingredientForRecipeDTO.ingredient()));
+        mappedIngredientForRecipe.setIngredientType(this.ingredientTypeService.getIngredient(ingredientForRecipeDTO.ingredient()));//here
         return mappedIngredientForRecipe;
     }
 
