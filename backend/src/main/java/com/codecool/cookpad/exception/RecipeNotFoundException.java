b15c81a81cb689956fc061bd224f5492dc479a0d
naaaani <nandi.zalka@gmail.com>
2024-05-14 14:16:59 +0200

    Add logging to ControllerAdvisor.

--- backend/src/main/java/com/codecool/cookpad/exception/RecipeNotFoundException.java
+++ backend/src/main/java/com/codecool/cookpad/exception/RecipeNotFoundException.java
@@ -1,8 +1,8 @@
 package com.codecool.cookpad.exception;
 
 public class RecipeNotFoundException extends RuntimeException{
-    private static final String MESSAGE = "Recipe not found";
-    public RecipeNotFoundException() {
-        super(MESSAGE);
+    private static final String MESSAGE = "Recipe not found with id: ";
+    public RecipeNotFoundException(String id) {
+        super(MESSAGE + id);
     }
 }
