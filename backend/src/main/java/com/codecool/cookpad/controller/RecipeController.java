3115b8420503ca974d1dd4396cd02b3c80f5adec
naaaani <nandi.zalka@gmail.com>
2024-06-03 10:54:54 +0200

    Remove doubled up methods in IngredientController, RecipeController

--- backend/src/main/java/com/codecool/cookpad/controller/RecipeController.java
+++ backend/src/main/java/com/codecool/cookpad/controller/RecipeController.java
@@ -3,8 +3,6 @@ package com.codecool.cookpad.controller;
 import com.codecool.cookpad.dto.RecipeDTO;
 import com.codecool.cookpad.service.RecipeService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
