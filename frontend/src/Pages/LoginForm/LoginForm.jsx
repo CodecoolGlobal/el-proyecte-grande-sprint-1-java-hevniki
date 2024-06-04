b7d496adb2a2bfb3523e43da61bf90148080a91e
naaaani <nandi.zalka@gmail.com>
2024-06-04 12:38:32 +0200

    Fix imports in LoginForm.jsx

--- frontend/src/Pages/LoginForm/LoginForm.jsx
+++ frontend/src/Pages/LoginForm/LoginForm.jsx
@@ -13,19 +13,7 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import axios from "axios";
 import "./LoginForm.css";
-import { CurrentUserContext } from '../../CurrentUserContext';
-import { useNavigate } from 'react-router-dom';
-import { Link as RouterLink } from 'react-router-dom';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CurrentUserContext 	} from '../../CurrentUserContext'
 
 function LoginForm() {
 	const navigate = useNavigate();
