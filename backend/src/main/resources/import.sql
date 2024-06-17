4a3ddc84ed082480fc3c9dbf9ec3ba8637b2d87e
naaaani <nandi.zalka@gmail.com>
2024-06-17 15:23:07 +0200

    Modify ingredient amounts in import.sql

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -3,7 +3,7 @@ INSERT INTO ingredient_type (id, name, category, approved)
 VALUES
     (1, 'Flour', 'WHEAT', true),
     (2, 'Milk', 'MILK_OR_DAIRY', true),
-    (3, 'Egg', 'EGG', true),
+    (3, 'Eggs', 'EGG', true),
     (4, 'Chicken', 'MEAT', true),
     (5, 'Butter', 'MILK_OR_DAIRY', true);
 
@@ -19,15 +19,15 @@ VALUES
 -- Inserting ingredients for recipes
 INSERT INTO ingredient_for_recipe (id, amount, ingredient_type_id, recipe_id)
 VALUES
-    (1, 300.0, 1, 1),  -- Flour for Vegan Pizza
-    (2, 500.0, 1, 2),  -- Flour for Gluten-Free Pancakes
-    (3, 300.0, 2, 2),  -- Milk for Gluten-Free Pancakes
-    (4, 2.0, 3, 2),    -- Egg for Gluten-Free Pancakes
-    (5, 400.0, 1, 3),  -- Flour for Vegetarian Lasagna
-    (6, 200.0, 5, 3),  -- Butter for Vegetarian Lasagna
-    (7, 500.0, 1, 4),  -- Flour for Chicken Alfredo Pasta
-    (8, 400.0, 4, 4),  -- Chicken for Chicken Alfredo Pasta
-    (9, 300.0, 2, 5),  -- Milk for Chocolate Cake
-    (10, 200.0, 1, 5), -- Flour for Chocolate Cake
-    (11, 150.0, 5, 5); -- Butter for Chocolate Cake
+    (1, '300 g', 1, 1),  -- Flour for Vegan Pizza
+    (2, '500 g', 1, 2),  -- Flour for Gluten-Free Pancakes
+    (3, '300 g', 2, 2),  -- Milk for Gluten-Free Pancakes
+    (4, '2', 3, 2),    -- Egg for Gluten-Free Pancakes
+    (5, '40 dkg', 1, 3),  -- Flour for Vegetarian Lasagna
+    (6, '200 g', 5, 3),  -- Butter for Vegetarian Lasagna
+    (7, '0.5 kg', 1, 4),  -- Flour for Chicken Alfredo Pasta
+    (8, '0.5 kg', 4, 4),  -- Chicken for Chicken Alfredo Pasta
+    (9, '30 dl', 2, 5),  -- Milk for Chocolate Cake
+    (10, '500 g', 1, 5), -- Flour for Chocolate Cake
+    (11, '15 dkg', 5, 5); -- Butter for Chocolate Cake
 
